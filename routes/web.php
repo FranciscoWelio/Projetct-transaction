3eb0a36c389e6f47ddfb2fdfe498f750f7affd25
Francisco Welio <fwelio.dev@gmail.com>
2025-04-14 22:00:31 -0300

    feat/ teste de bancoi

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,20 @@ Route::get('/', function () {
 Route::apiResource('users',controller: UserController::class);
 Route::apiResource('merchants', MerchantController::class);
 Route::apiResource('transactions', TransactionController::class);
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/check-db', function() {
+    try {
+        DB::connection()->getPdo();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Conectado ao SQLite: ' . DB::connection()->getDatabaseName()
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Erro na conexão: ' . $e->getMessage()
+        ], 500);
+    }
+});
